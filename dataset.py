70b12059bac5c697d14955f6bec44b7bbe7a610e
user2812 <gassama761@gmail.com>
2026-06-14 17:47:22 +0000

    feat: Implémentation dataset, model et utils

--- dataset.py
+++ dataset.py
@@ -180,7 +180,7 @@ def get_dataloaders(
 if __name__ == "__main__":
     from transformers import BertTokenizer
 
-    CSV_PATH  = "data/train.csv"
+    CSV_PATH  = "data/train-3.csv"
     tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
 
     train_loader, valid_loader = get_dataloaders(
